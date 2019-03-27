22eb28366a402ca5e7a02a6766a6cf7aeec823ca
Mark Allred <markjallred@gmail.com>
2019-03-27 12:52:11 -0600

    Shifted pre-test environment into new folder and moved in test folder

---
+++ pre_test_2/midterm_toolkit.py
@@ -0,0 +1,242 @@
+#############################################################
+# module: midterm_toolkit.py
+# Mark Allred
+# A01647260
+##############################################################
+
+from prod import prod
+from plus import plus
+from quot import quot
+from const import const
+from maker import make_const, make_prod, make_quot, make_pwr, make_plus
+from maker import make_pwr_expr
+from deriv import deriv
+from tof import tof
+from antideriv import antideriv, antiderivdef
+from hw08_s19 import amplify_grayscale_blur_img_dir
+import numpy as np
+from scipy import stats
+import scipy as sp
+import cv2
+import math as m
+from defintegralapprox import midpoint_rule, trapezoidal_rule, simpson_rule
+from riemann import riemann_approx
+
+
+def demand_elasticity_function(demand_curve=None, returnPyFunc=False):
+    '''
+    Creates and returns the Elasticity of Demand function 
+    for a given demand_curve. Returns a functional
+    representation by default or a Python function if
+    returnPyFunc = True.
+
+    demand_curve - functional representation of a demand curve, f(p)
+
+    returnPyFunc - Function returns Functional Representation if False, Python function if True
+    '''
+
+    assert demand_curve is not None
+
+    # E(p) = (-p * f'(p)) / f(p)
+    numerator = make_prod(mult_expr1 = make_prod(make_const(-1.0), make_pwr('x', 1.0)),
+                          mult_expr2 = deriv(demand_curve))
+    denominator = demand_curve
+    demand_elasticity = make_quot(nexpr = numerator, dexpr = denominator)
+
+    if returnPyFunc:
+        demand_elasticity = tof(demand_elasticity)
+    return demand_elasticity
+
+
+def demand_elasticity(demand_curve, price):
+    '''
+    Returns const object representing the elasticity of demand 
+    at the price for the given demand cuve.
+    
+    demand_curve - functional representation of a demand curve, f(p)
+
+    price - const object representing the price demanded
+    '''
+    assert demand_curve is not None
+    assert isinstance(price, const)
+    elasticityFunc = demand_elasticity_function(demand_curve=demand_curve, 
+                                                returnPyFunc=True)
+    elasticity = elasticityFunc(price.get_val())
+    return make_const(elasticity)
+
+
+def is_demand_elastic(demand_curve, price):
+    '''
+    Returns Boolean whether the quantity demanded is elastic or inelastic
+
+    demand_curve - functional representation of a demand curve, f(p)
+
+    price - const object representing the price demanded
+    '''
+    assert isinstance(price, const)
+    if demand_elasticity(demand_curve, price).get_val() > 1:
+        return True
+    else:
+        return False
+
+
+def grayscale_images_in_dir(ftype, in_img_dir):
+    '''
+    Function grayscales each image in a directory with the given file type
+
+    ftype - A string specifiying thefFile type for image, e.g. '.jpg', '.png'.
+
+    in_img_dir - String directory location for the given images, e.g. '/home/vladimir/images/'.
+    '''
+    amplify_grayscale_blur_img_dir(ftype=ftype, in_img_dir=in_img_dir, kz=0, c='b', amount=0, appendTitle='_gray')
+
+
+def split_channel_stats(filePath):
+    '''
+    Function splits an image into color channels and computes the mean, median, 
+    mode, and standard deviation for each color channel. Returns a list of 5-tuples
+    that includes the color channel, average, median, mode, and standard deviation
+    of that channel. List order is Blue, Green, and then Red.
+
+    filePath - String representing the path to the file
+    '''
+    
+    # Read in file
+    imgArr = cv2.imread(filePath)
+    assert imgArr is not None
+
+    # Split into channels 
+    b,g,r = cv2.split(imgArr)
+    channels = [b, g, r]
+    colorStats = []
+
+    # Compute statistics and append the tuple to return list
+    for color in channels:
+        avg = np.mean(color)    # Default is whole array, not an axis
+        med = np.median(color)  # Default is whole array, not an axis
+        mod = stats.mode(color, axis=None) # Default is each axis, axis=None -> whole array
+        sd  = np.std(color)     # Default is whole array, not an axis
+        colorStats.append( (color, avg, med, mod, sd) ) # Append tuple
+    
+    return colorStats
+
+
+def grayscale_blur_and_stats(filePath, blurSize=3):
+    '''
+    Function grayscales and blurs a specific image then computes statistics on the image. 
+    Returns tuple of the image array, average, median, mode, and standard deviation.
+
+    filePath - String representing the path to the file
+
+    blurSize - Odd positive integer specifying the size of a mean blur filter.
+    '''
+    
+    # Read in image
+    imgArr = cv2.imread(filePath)
+    assert imgArr is not None
+    
+    # Grayscale and blur image
+    grayImg = cv2.cvtColor(imgArr, cv2.COLOR_RGB2GRAY)
+    blurImg = cv2.blur(grayImg, (blurSize, blurSize))
+    
+    # Compute statistics
+    avg = np.mean(blurImg)    # Default is whole array, not an axis
+    med = np.median(blurImg)  # Default is whole array, not an axis
+    mod = stats.mode(blurImg, axis=None) # Default is each axis, axis=None -> whole array
+    sd  = np.std(blurImg)     # Default is whole array, not an axis
+
+    return (blurImg, avg, med, mod, sd)
+
+def amplify_and_display(filePath, color, amount):
+    '''
+    Amplifies a color channel within specific image then displays both the 
+    amplified and original image.
+
+    filePath - String representing the path to the file
+
+    color - String / char representing the color channel to amplify
+
+    amount - Integer specifying the amount to amplify
+    '''
+
+    # Read in image
+    imgArr = cv2.imread(filePath)
+    assert imgArr is not None
+    
+    # Split channels for amplication
+    b,g,r = cv2.split(imgArr)
+
+    # Amplify specified color
+    if color=='b' or color=='B':
+        b+=amount
+    elif color=='g' or color=='G':
+        g+=amount
+    elif color=='r' or color=='R':
+        r+=amount
+    else:
+        print("Invalid color provided. Exiting function.")
+        return
+
+    # Combine channels to create image
+    amplifiedImage = cv2.merge([b,g,r])
+    # Show original and amplified image
+    cv2.imshow(filePath, imgArr)
+    cv2.imshow("Amplified", amplifiedImage)
+
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
+def net_change(marginalRevExpr, pl1, pl2):
+    assert isinstance(pl1, const)
+    assert isinstance(pl2, const)
+
+    revenueNetChange = antiderivdef(expr=marginalRevExpr, a=pl1, b=pl2)
+    return make_const(revenueNetChange) # NOT SURE IF WE NEED TO RETURN CONST?
+
+
+def taylor_poly(fexpr, a, n):
+    assert isinstance(a, const)
+    assert isinstance(n, const)
+
+    taylorFunc = nthTaylor(fexpr, a, n, 0)
+    for i in range(1, n.get_val()+1):
+        fexpr = deriv(fexpr)
+        nextTaylor = nthTaylor(fexpr, a, n, i)
+        taylorFunc = make_plus(elt_expr1=taylorFunc, elt_expr2=nextTaylor)
+    print(taylorFunc)
+    return tof(taylorFunc)
+
+
+def nthTaylor(expr, a, n, i):
+    exprFunc = tof(expr)
+    exprAtA = exprFunc(a.get_val())
+    frac = exprAtA / m.factorial(i)
+    
+    xMinusA = make_plus(elt_expr1=make_pwr('x', 1.0),
+                        elt_expr2=make_const(-1 * a.get_val()))
+    nthtaylor = make_prod(mult_expr1=make_const(frac),
+                          mult_expr2=make_pwr_expr(expr=xMinusA, deg=i))
+    return nthtaylor
+    
+
+def olsComparison(filePath):
+    data = sp.genfromtxt(filePath, delimiter='\t')
+    # Split data into x,y vectors
+    x = data[:,0] 
+    y = data[:,1]
+
+    # Remove any NaN's
+    if sp.sum(sp.isnan(y)) > 0: 
+        x = x[~sp.isnan(y)]
+        y = y[~sp.isnan(y)]
+
+    pcoeffs, error = sp.polyfit(x, y, 1)
+    f = sp.poly1d(sp.polyfit(x, y, 1))
+    print('Model coeffs: %s' % pcoeffs)
+    print('Model error: %s' % err)
+    print('Our error: %s' % error(f, x, y))
+
+
+def SSE(f, x, y):
+    return sp.sum((f(x) - y) ** 2)
---
+++ pre_test_2/riemann.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python
+
+###############################################
+# module: riemann.py
+# Mark Allred
+# A01647260
+###############################################
+
+## modify these imports as you see fit.
+import numpy as np
+from const import const
+from antideriv import antiderivdef
+from tof import tof
+import matplotlib.pyplot as plt
+from maker import make_const
+
+def riemann_approx(fexpr, a, b, n, pp=0):
+  '''
+  pp=0 - approximate with reimann midpoint
+  pp=+1 - approximate with reimann right point
+  pp=-1 - approximate with reiman left point
+  '''
+  assert isinstance(a, const)
+  assert isinstance(b, const)
+  assert isinstance(n, const)
+  fexprf = tof(fexpr)
+  assert(fexprf) is not None
+  dx = (b.get_val() - a.get_val()) / n.get_val()
+
+  
+  if pp == 0:
+    return riemann_midpoint(fexprf, a, b, n, dx)
+  elif pp == 1:
+    return riemann_right(fexprf, a, b, n, dx)
+  elif pp == -1:
+    return riemann_left(fexprf, a, b, n, dx)
+  else:
+    print("Incorrect Partition Point provided", pp)
+
+
+def riemann_midpoint(fexpr, a, b, n, dx):
+  a = a.get_val()
+  xvals1 = np.array([a + i * dx for i in range(n.get_val())], dtype=np.float32)
+  xvals2 = np.array([a + i * dx for i in range(1, n.get_val()+1)], dtype=np.float32) 
+  assert xvals1.size == xvals2.size
+  xvals = np.array([0.5 * (xvals1[i] + xvals2[i]) for i in range(xvals1.size)], dtype=np.float32)
+  yvals = np.array([fexpr(x) for x in xvals], dtype=np.float32)
+
+  return make_const(np.sum(yvals) * dx)
+
+
+def riemann_left(fexpr, a, b, n, dx):
+  a = a.get_val()
+  xvals = np.array([a + i * dx for i in range(n.get_val())], dtype=np.float32)
+  yvals = np.array([fexpr(x) for x in xvals], dtype=np.float32)
+  return make_const(sum(yvals) * dx)
+
+
+def riemann_right(fexpr, a, b, n, dx):
+  a = a.get_val()
+  xvals = np.array([a + i * dx for i in range(1, n.get_val()+1)], dtype=np.float32)
+  yvals = np.array([fexpr(x) for x in xvals])
+  return make_const(sum(yvals) * dx)
+
+
+def riemann_approx_with_gt(fexpr, a, b, gt, n_upper, pp=0):
+  assert isinstance(a, const)
+  assert isinstance(b, const)
+  assert isinstance(gt, const)
+  assert isinstance(n_upper, const)
+
+  if n_upper.get_val() < 0:
+    print("Negative upper bound provided, using absolute value...")
+    n_upper = make_const(abs(n_upper.get_val()))
+
+  # initialize empty array
+  errorAndValue = [0] * n_upper.get_val()
+  
+  for i in range(1, n_upper.get_val()+1):
+    approx = riemann_approx(fexpr, a, b, make_const(i), pp)
+    errorAndValue[i-1] = (i, make_const(abs(approx.get_val() - gt.get_val())))
+  
+  return errorAndValue
+  
+
+def plot_riemann_error(fexpr, a, b, gt, n):
+  assert isinstance(a, const)
+  assert isinstance(b, const)
+  assert isinstance(gt, const)
+  assert isinstance(n, const)
+  
+  # Partion Points for left, mid, & right Riemann Approximation
+  ppL = -1.0
+  ppM =  0.0
+  ppR = +1.0
+
+  # Generate error approximations for the given expression, bounds, actual value, and upper bound on subintervals
+  leftApprox  = riemann_approx_with_gt(fexpr, a, b, gt, n, ppL)
+  midApprox   = riemann_approx_with_gt(fexpr, a, b, gt, n, ppM)
+  rightApprox = riemann_approx_with_gt(fexpr, a, b, gt, n, ppR)
+  
+  # Create x-values for graph
+  xvals = np.linspace(1, n.get_val()+1, n.get_val())
+  # Create y-values for each error by slicing out error values for each approximation
+  leftErr = [err[1].get_val() for err in leftApprox]
+  midErr = [err[1].get_val() for err in midApprox]
+  rightErr = [err[1].get_val() for err in rightApprox]
+
+  fig = plt.figure(1)
+  fig.suptitle('Riemann Approximation Error')
+  plt.xlabel('n')
+  plt.ylabel('err')
+  plt.xlim(0, n.get_val())
+  plt.grid()
+  plt.plot(xvals, leftErr, label = 'left', c = 'g')
+  plt.plot(xvals, midErr, label = 'mid', c = 'r')
+  plt.plot(xvals, rightErr, label = 'right', c = 'b')
+  plt.legend(loc = 'best')
+  plt.show()
+
+
+  
